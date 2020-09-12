8ef75bd184bb1afa794c1255c6ae9be75a936003
maxgmona <67889564+maxgmona@users.noreply.github.com>
2020-09-11 21:00:53 -0500

    Creacion de las variables de la clase cliente

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,9 +1,58 @@
 package modelo;
 
-/**
- *
- * @author jojofrei
- */
+import java.util.ArrayList;
+
 public class Cliente {
+    private int numeroCliente;
+    private int paciencia;
+    private ArrayList<Producto> productosOrdenados;
+    private boolean ordenServida;
+    private String nombreImagen;
+    private boolean mostrarEnJuego;
+
+    public Cliente()
+    {
+        
+    }
     
+    public Cliente(int numeroCliente, int paciencia, ArrayList<Producto> productos, 
+                   boolean ordenServida, String nombreImagen, boolean mostrarEnJuego)
+    {
+        this.numeroCliente       = numeroCliente;
+        this.paciencia           = paciencia;
+        this.productosOrdenados  = productos;
+        this.ordenServida        = ordenServida;
+        this.nombreImagen        = nombreImagen;
+        this.mostrarEnJuego      = mostrarEnJuego;
+    }
+    
+    public int getNumeroCliente()
+    {
+        return this.numeroCliente;
+    }
+    
+    public int getPaciencia()
+    {
+        return this.paciencia;
+    }
+    
+    public ArrayList<Producto> getProductosOrdenados()
+    {
+        return this.productosOrdenados;
+    }
+    
+    public String getNombreImagen()
+    {
+        return this.nombreImagen;
+    }
+    
+    public void setPaciencia(int paciencia)
+    {
+        this.paciencia = paciencia;
+    }
+    
+    public void setOrdenServida(boolean estado)
+    {
+        this.ordenServida = estado;
+    }        
 }
