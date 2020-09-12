859002a6e2c43461664a90b56d0dcec3a4cb1c3a
maxgmona <67889564+maxgmona@users.noreply.github.com>
2020-09-11 21:01:51 -0500

    desarrollo de la clase juego

--- src/modelo/Juego.java
+++ src/modelo/Juego.java
@@ -1,9 +1,51 @@
 package modelo;
 
-/**
- *
- * @author jojofrei
- */
-public class Juego {
+import java.util.ArrayList;
+
+public class Juego 
+{
+    private String nombreUsuario;
+    private int nivelEnCurso;
+    private ArrayList<Cliente> listaClientes;
+    private double dineroAcumulado;
     
+    public Juego()
+    {
+        
+    }
+        
+    public Juego(String nombreUsuario, int nivelEnCurso,ArrayList<Cliente> clientes, double dineroAcumulado)
+    {
+        this.nombreUsuario   = nombreUsuario;
+        this.nivelEnCurso    = nivelEnCurso;
+        this.listaClientes   = clientes;
+        this.dineroAcumulado = dineroAcumulado;
+    }
+    
+    public Juego(String nombreUsuario, int nivelEnCurso, double dineroAcumulado)
+    {
+        this.nombreUsuario   = nombreUsuario;
+        this.nivelEnCurso    = nivelEnCurso;        
+        this.dineroAcumulado = dineroAcumulado;
+    }
+    
+    public String getNombreUsuario()
+    {
+        return this.nombreUsuario;
+    }
+    
+    public int getNivelEnCurso()
+    {
+        return this.nivelEnCurso;
+    }
+    
+    public ArrayList<Cliente> getListaClientes()
+    {
+        return this.listaClientes;
+    }
+    
+    public double getDineroAcumulado()
+    {
+        return this.dineroAcumulado;
+    }      
 }
