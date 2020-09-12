9967593ef5a205df76c8d8bf9d4a79045c4ea2f9
isaac22arce <67889523+isaac22arce@users.noreply.github.com>
2020-09-12 16:06:27 -0500

    edicion de PantallaPrincipal y PantallaPrincipalControler

--- src/controlador/PantallaPrincipalController.java
+++ src/controlador/PantallaPrincipalController.java
@@ -7,7 +7,14 @@ package controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +23,69 @@ import javafx.fxml.Initializable;
  */
 public class PantallaPrincipalController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+     @FXML
+    private Button btn_ComenzarJuego;
+    @FXML
+    private Button btn_VerTablaPuntaje;
+    @FXML
+    private Button btn_SalirJuego;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }   
+    
+    @FXML
+    private void comenzarJuego(ActionEvent event) 
+    {
+        try 
+        {
+            //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vista/PantallaJuegoCurso.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vista/PantallaJuego.fxml"));            
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            //stage.setTitle("Juego Restaurante");            
+            stage.setTitle("Selección de usuario");            
+            stage.setScene(new Scene(root));
+            //stage.setWidth(1060);
+            stage.setWidth(400);
+            //stage.setResizable(false);
+            //stage.setHeight(750);
+            stage.setHeight(200);
+            stage.show();            
+        } 
+        catch(Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    @FXML
+    private void verTablaPuntajes(ActionEvent event) 
+    {
+        try 
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vista/TablaPuntajes.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Tabla de Puntajes");
+            stage.setScene(new Scene(root));  
+            stage.show();            
+        } 
+        catch(Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void salirJuego(ActionEvent event) 
+    {
+        Stage stage = (Stage) btn_SalirJuego.getScene().getWindow();        
+        stage.close();
+    }
     
 }
+    
+
--- src/helper/HelperArchivo.java
+++ src/helper/HelperArchivo.java
@@ -5,10 +5,41 @@
  */
 package helper;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 /**
  *
  * @author jojofrei
  */
 public class HelperArchivo {
+    //private static String rutaGeneral = Paths.get("").toAbsolutePath().toString();
+    //public static String rutaArchivo = rutaGeneral+"/src/archivo";          
+    //public static String rutaImagenesCliente = rutaGeneral+"/src/recursos/clientes";          
+    //public static String rutaImagenesProductos = rutaGeneral+"/src/recursos/productos"; 
+    
+     public static ArrayList<String> leerLineasArchivo(String name)
+    {    
+        ArrayList<String> lineasArchivo = new ArrayList<>();        
+        BufferedReader reader;
+        try
+        {
+            reader = new BufferedReader(new FileReader(name));
+            String line = reader.readLine();
+            while (line != null) 
+            {    
+                lineasArchivo.add(line);
+                // read next line
+                line = reader.readLine();                
+            }
+            reader.close();
+        } catch (IOException e) {
+                e.printStackTrace();
+        }     
+        return lineasArchivo;                 
+    }
     
 }
