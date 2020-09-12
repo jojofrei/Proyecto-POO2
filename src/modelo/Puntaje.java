431f95c5b51b0aedaa321d87336684cbcdc768b1
maxgmona <67889564+maxgmona@users.noreply.github.com>
2020-09-12 00:27:58 -0500

    Desarrollo de la clase puntaje, se termino con el paquete modelo

--- src/modelo/Puntaje.java
+++ src/modelo/Puntaje.java
@@ -1,9 +1,34 @@
 package modelo;
 
-/**
- *
- * @author jojofrei
- */
 public class Puntaje {
+    private String jugador;
+    private int nivel;
+    private double puntaje;
     
+    public Puntaje()
+    {
+        
+    }
+    
+    public Puntaje(String jugador, int nivel, double puntaje)
+    {
+        this.jugador = jugador;
+        this.nivel   = nivel;
+        this.puntaje = puntaje;
+    }
+    
+    public String getJugador()
+    {
+        return this.jugador;
+    }
+    
+    public int getNivel()
+    {
+        return this.nivel;
+    }
+    
+    public double getPuntaje()
+    {
+        return this.puntaje;
+    }            
 }
