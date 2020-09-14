b9608d81327c3af299f9ff8920fba2d03588e1c8
maxgmona <67889564+maxgmona@users.noreply.github.com>
2020-09-14 01:17:28 -0500

    Update HelperJuego.java
    continuacion de la clase helperjuego

--- src/helper/HelperJuego.java
+++ src/helper/HelperJuego.java
@@ -117,4 +117,120 @@ public class HelperJuego
                 listaProductosCategoria.add(producto);            
         }       
         return listaProductosCategoria;
+    }public static ArrayList<Cliente> getListaRandomClientes(ArrayList<Producto> listaProductos)
+    {
+        ArrayList<Cliente> listaClientes = new ArrayList<>();
+        //Generacion de cantidad de clientes
+        int cantClientes  = 1 + new Random().nextInt(3);
+        int cantMaximaClientes = Constantes.CANT_MAX_CLIENTES_MOSTRAR;
+                
+        int numeroCliente,pacienciaCliente;
+        String nombreImagen;
+        boolean ordenServida = false;
+        boolean mostrarEnJuego = false;
+        ArrayList<Producto> listaProductosOrdenados;
+        for(int i = cantClientes; i<= cantMaximaClientes ; i++)
+        {            
+            //Generacion de datos del cliente
+            numeroCliente = i;
+            
+            //Generacion de paciencia del cliente            
+            pacienciaCliente = 3 + new Random().nextInt(Constantes.MAXIMA_PACIENCIA_CLIENTE);            
+            
+            listaProductosOrdenados = getListaProductosOrdenados(listaProductos);            
+            nombreImagen = "cli"+i+".png";
+            listaClientes.add(new Cliente(numeroCliente,
+                                          pacienciaCliente,
+                                          listaProductosOrdenados,
+                                          ordenServida,
+                                          nombreImagen,
+                                          mostrarEnJuego));
+        }        
+        return listaClientes;
     }
+        
+    
+    public static ArrayList<Producto> getListaProductosOrdenados(ArrayList<Producto> listaProductos)
+    {
+        //Generacion de Cantidad de opciones del menu
+        int opcionesMenu = 1 + new Random().nextInt(3);
+        int cantMaximaOpcionesMenu = Constantes.CANT_MAX_OPCIONES_MENU;
+        
+        //Lista de Productos Totales        
+        int cantTotalProductos = listaProductos.size();        
+        ArrayList<Producto> listaProductosOrdenados = new ArrayList<>();
+        int indiceProductoGenerado;
+        for(int i = opcionesMenu ; i <= cantMaximaOpcionesMenu ; i++)
+        {
+            //Generar producto Aleatorio
+            indiceProductoGenerado = new Random().nextInt(cantTotalProductos);
+            
+            if(!listaProductosOrdenados.contains(listaProductos.get(indiceProductoGenerado)))
+                listaProductosOrdenados.add(listaProductos.get(indiceProductoGenerado));
+        }
+        return listaProductosOrdenados;
+    }
+    
+    public static ArrayList<Producto> getListaProductos(String rutaProductos)
+    {
+        ArrayList<Producto> listaProductos = new ArrayList<>();
+        ArrayList<String> listaProductosArchivo = helper.HelperArchivo.leerLineasArchivo(rutaProductos);
+        String nombreProducto,categoriaProducto,urlProducto = "";
+        for(String productoString : listaProductosArchivo)
+        {            
+            String[] productoPart = productoString.split(",");
+            nombreProducto       = productoPart[2];
+            categoriaProducto    = productoPart[1];
+            urlProducto          = productoPart[0];
+            listaProductos.add(new Producto(nombreProducto, categoriaProducto, urlProducto));                        
+        }
+        return listaProductos;
+    }
+    
+    public static boolean verificarEstadoOrdenServida(ArrayList<Producto> listaProductosCliente, ArrayList<Producto> listaProductosCocina)
+    {
+        boolean estadoOrdenServida = false;
+        
+        System.out.println("productos cliente");
+        for(Producto producto: listaProductosCliente)
+            System.out.println("nombre="+producto.getNombre()+",categoria="+producto.getCategoria());
+        System.out.println("");
+        System.out.println("productos cocinandose");
+        for(Producto producto: listaProductosCocina)
+            System.out.println("nombre="+producto.getNombre()+",categoria="+producto.getCategoria());
+                                
+        if(listaProductosCocina.containsAll(listaProductosCliente))
+            estadoOrdenServida = true;
+        return estadoOrdenServida;
+        
+    }
+    
+    public static int chequearSiEsSiguienteNivel(double dineroAcumulado,int nivel)
+    {
+        int siguienteNivel = 0;
+        switch(nivel)
+        {
+            case 1:
+                if(dineroAcumulado > Constantes.CANT_MAX_DINERO_GANADO_MENU_NIVEL1)
+                    siguienteNivel = 2;
+                else
+                    siguienteNivel = nivel;
+                break;
+                
+            case 2:
+                if(dineroAcumulado > Constantes.CANT_MAX_DINERO_GANADO_MENU_NIVEL2)
+                    siguienteNivel = 3;
+                else
+                    siguienteNivel = nivel;
+                break;
+                
+            case 3:
+                if(dineroAcumulado > Constantes.CANT_MAX_DINERO_GANADO_MENU_NIVEL3)
+                    siguienteNivel = 4;
+                else
+                    siguienteNivel = nivel;
+                break;
+        }
+        return siguienteNivel;
+    }
+    
