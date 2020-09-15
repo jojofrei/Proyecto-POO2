7ae94cdbdf6919a9bfb9cbf58b319a02185e7ed8
jojofrei <jojofrei@espol.edu.ec>
2020-09-14 20:20:33 -0500

    Pantalla juego controller

--- src/controlador/PantallaJuegoController.java
+++ src/controlador/PantallaJuegoController.java
@@ -5,12 +5,19 @@
  */
 package controlador;
 
+import helper.HelperJuego;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import modelo.Juego;
 
 /**
  * FXML Controller class
@@ -30,6 +37,33 @@ public class PantallaJuegoController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }       
+    }     
+    
+    @FXML
+    private void comenzarJuego(ActionEvent event) 
+    {
+        try 
+        {            
+            Juego juegoActual = HelperJuego.validarIngresoJugador(txt_usuario.getText().trim());            
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vista/PantallaJuegoCurso.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Juego comenzado: "+juegoActual.getNombreUsuario());
+            stage.setScene(new Scene(root));  
+            stage.setWidth(1060);            
+            stage.setResizable(false);
+            stage.setHeight(750);
+            PantallaJuegoCursoController controlador = fxmlLoader.<PantallaJuegoCursoController> getController();
+            controlador.cargarJuego(juegoActual.getNombreUsuario(),juegoActual.getNivelEnCurso());
+            stage.show();            
+            Stage stageActual = (Stage) btn_Jugar.getScene().getWindow();            
+            stageActual.close();
+            
+        } 
+        catch(Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }    
     
 }
