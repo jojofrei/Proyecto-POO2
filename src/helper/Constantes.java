92d90faba7d5e592264d5fd139269407bcace925
jojofrei <jojofrei@espol.edu.ec>
2020-09-15 21:11:30 -0500

    Aumento del dinero para pasar de nivel

--- src/helper/Constantes.java
+++ src/helper/Constantes.java
@@ -6,9 +6,9 @@ public class Constantes {
     public static final int CANT_MAX_OPCIONES_MENU_NIVEL1 = 4;
     public static final int CANT_MAX_OPCIONES_MENU_NIVEL2 = 5;
     public static final int CANT_MAX_OPCIONES_MENU_NIVEL3 = 6;    
-    public static final int CANT_MAX_DINERO_GANADO_MENU_NIVEL1 = 100;
-    public static final int CANT_MAX_DINERO_GANADO_MENU_NIVEL2 = 150;
-    public static final int CANT_MAX_DINERO_GANADO_MENU_NIVEL3 = 200;
+    public static final int CANT_MAX_DINERO_GANADO_MENU_NIVEL1 = 350;
+    public static final int CANT_MAX_DINERO_GANADO_MENU_NIVEL2 = 500;
+    public static final int CANT_MAX_DINERO_GANADO_MENU_NIVEL3 = 800;
     public static final int MAXIMA_PACIENCIA_CLIENTE = 20;
     public static final int MAXIMA_CANTIDAD_CLIENTES_PERDIDOS_JUEGO_ACABADO = 5;
     public static final String categoriasProductos = "asiatica,bebida,postre,rapida";
